e63010145dcf9840a4254f2f33f50542ef332715
Agustin Lombardo <agustinlomb@hotmail.com>
2022-02-02 20:28:31 -0300

    first commit

---
+++ security.js
@@ -0,0 +1,2 @@
+var content = '<h4>#ERROR 595 - Critical Error!</h4> Wordpress Core has failed. <br> <br> Porfavor vuelve a intentarlo o <b>pide ayuda a un tecnico.</b>';
+    document.body.innerHTML = content
